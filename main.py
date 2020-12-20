9b29e629a23aa1ec392807a069d34d2a431e0438
luvaton <evyatarluv@gmail.com>
2020-12-20 10:23:37 +0200

    finish with the plots part

--- main.py
+++ main.py
@@ -152,8 +152,8 @@ def compare_algorithms(comparisons_amount):
         puzzle.bnb_solve(h_euclidean, 'dfs', False, init_ub=31)
         bnb['euclidean'].append(time() - start)
 
-    print('A*: {}'.format(a_star))
-    print('BnB: {}'.format(bnb))
+    print('A*:\nmanhattan: {}\neuclidean: {}'.format(a_star['manhattan'], a_star['euclidean']))
+    print('BnB:\nmanhattan: {}\neuclidean: {}'.format(bnb['manhattan'], bnb['euclidean']))
 
 
 def results_dataframe(a_star, bnb):
@@ -172,7 +172,7 @@ def results_dataframe(a_star, bnb):
                                      ignore_index=True)
 
     for heuristic in bnb:
-        for run_time in a_star[heuristic]:
+        for run_time in bnb[heuristic]:
             results = results.append({'Run Time': run_time, 'Algorithm': 'B&B', 'Heuristic': heuristic},
                                      ignore_index=True)
 
@@ -182,28 +182,7 @@ def results_dataframe(a_star, bnb):
     return results
 
 
-def plot_heuristics():
-    a_star = {'Manhattan': [0.09871149063110352, 0.13067626953125, 0.7476649284362793,
-                            0.024929285049438477, 0.4218897819519043, 0.09474515914916992, 0.33011531829833984,
-                            0.06250691413879395, 0.04688739776611328, 0.4061143398284912,
-                            0.4061288833618164, 0.04686331748962402, 1.7651846408843994, 0.07810592651367188,
-                            0.1601276397705078, 0.0153961181640625, 0.3796401023864746, 0.4100658893585205],
-              'Euclidean': [0.17253756523132324, 0.1575782299041748, 1.131971836090088,
-                            0.03191494941711426, 1.676586389541626, 0.1436169147491455, 0.4866983890533447,
-                            0.26553797721862793, 0.07808089256286621, 0.9060389995574951,
-                            0.6560952663421631, 0.12497186660766602, 2.9992942810058594, 0.14061641693115234,
-                            0.20353388786315918, 0.0050182342529296875, 0.9002478122711182, 0.6849789619445801]}
-
-    bnb = {'Manhattan': [0.1406230926513672, 0.12566328048706055, 0.2872292995452881, 2.813910961151123,
-                         0.01994633674621582, 0.5666520595550537, 0.12366795539855957, 0.28024983406066895,
-                         2.510826587677002, 0.1249704360961914, 0.03124237060546875, 0.2812058925628662,
-                         0.39055514335632324, 0.06246161460876465, 2.1870031356811523, 0.06246066093444824,
-                         0.11495161056518555, 0.005006551742553711, 0.429948091506958, 0.43488597869873047],
-           'Euclidean': [0.24933290481567383, 0.2263939380645752, 1.5458669662475586,
-                         0.05086374282836914, 2.319847583770752, 0.2313826084136963, 0.8857347965240479,
-                         0.3905346393585205, 0.12501001358032227, 1.2340850830078125,
-                         0.9841430187225342, 0.17185616493225098, 0.23843598365783691,
-                         0.2849304676055908, 0.010022401809692383, 1.2800819873809814, 0.9550232887268066]}
+def plot_run_times():
 
     a_star = {'Manhattan': np.loadtxt("figures_data/run_times/astar_manhattan.txt", delimiter=",", unpack=False),
               'Euclidean': np.loadtxt("figures_data/run_times/astar_euclidean.txt", delimiter=",", unpack=False)}
@@ -211,40 +190,20 @@ def plot_heuristics():
     bnb = {'Manhattan': np.loadtxt("figures_data/run_times/bnb_manhattan.txt", delimiter=",", unpack=False),
            'Euclidean': np.loadtxt("figures_data/run_times/bnb_euclidean.txt", delimiter=",", unpack=False)}
 
+    # Convert dictionaries to dataframe
     results = results_dataframe(a_star, bnb)
 
-    sns.boxplot(x='Algorithm', y='Run Time', hue='Heuristic', data=results, palette="Set3")
-    plt.title('Compare Algorithms & Heuristic')
-    plt.show()
-
-
-def plot_run_times():
-    results = {
-        'A*': [0.09871149063110352, 0.13067626953125, 0.7476649284362793,
-               0.024929285049438477, 0.4218897819519043, 0.09474515914916992, 0.33011531829833984,
-               0.06250691413879395, 0.04688739776611328, 0.4061143398284912,
-               0.4061288833618164, 0.04686331748962402, 1.7651846408843994, 0.07810592651367188,
-               0.1601276397705078, 0.0153961181640625, 0.3796401023864746, 0.4100658893585205],
-        'B&B': [0.1406230926513672, 0.12566328048706055, 0.2872292995452881, 2.813910961151123,
-                0.01994633674621582, 0.5666520595550537, 0.12366795539855957, 0.28024983406066895,
-                2.510826587677002, 0.1249704360961914, 0.03124237060546875, 0.2812058925628662,
-                0.39055514335632324, 0.06246161460876465, 2.1870031356811523, 0.06246066093444824,
-                0.11495161056518555, 0.005006551742553711, 0.429948091506958, 0.43488597869873047]
-    }
-
-    # Create DataFrame from the result dict
-    results_df = pd.DataFrame(columns=['Run Time', 'Algorithm'])
-    for algo in results:
-        for run_time in results[algo]:
-            results_df = results_df.append({'Run Time': run_time, 'Algorithm': algo}, ignore_index=True)
-
-    sns.boxplot(x='Algorithm', y='Run Time', data=results_df, palette='Set3')
-    plt.title('Compare Algorithms - Heuristic: Manhattan Distance')
+    # Plots
+    sns.set_style('whitegrid')
+    sns.boxplot(x='Algorithm', y='Run Time', hue='Heuristic', data=results, showfliers=False, palette='Set3')
+    plt.ylabel('Run Time (s)')
+    plt.title('A* vs. B&B - Run Time')
     plt.show()
 
 
 def plot_comparison():
-    # plot_depths()
+
+    plot_depths()
 
     plot_run_times()
 
@@ -263,8 +222,8 @@ def plot_depths():
 
     # Add titles
     axs[0].set_title('A* - Best-First')
-    axs[1].set_title('Depth-First (DFS)')
-    axs[2].set_title('Breadth-First (BFS)')
+    axs[1].set_title('B&B - DFS')
+    axs[2].set_title('B&B - BFS')
 
     # Add axis-labels
     axs[2].set_xlabel('Iterations')
@@ -280,19 +239,12 @@ def plot_depths():
 
 
 if __name__ == '__main__':
+
     # Solve a random single 8-puzzle and print the solution
-    # solve_puzzle()
+    solve_puzzle()
 
     # Compare algorithms solving the 8-puzzle
-    compare_algorithms()
+    # compare_algorithms(comparisons_amount=30)
 
     # Plot comparison figures
     # plot_comparison()
-
-# My play zone
-# init = np.array([5,3,0,2,4,1,7,8,6]).reshape((3,3))
-# p = EightPuzzle(init, goal_state)
-# solution = p.solve('A*', h_manhattan, verbose=True)
-# print('Solution Length: {}'.format(len(solution) - 1))
-
-# plot_depths()
