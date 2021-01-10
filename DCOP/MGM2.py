2ac1c0bf04a5937d26d1bfa63266e17db4044e4e
luvaton <evyatarluv@gmail.com>
2021-01-10 22:01:44 +0200

    Add: docstrings for iteration #1, #2 methods

--- DCOP/MGM2.py
+++ DCOP/MGM2.py
@@ -4,103 +4,6 @@ import numpy as np
 from typing import List, Dict, Callable, Any
 
 
-class MGM2(Agent):
-    """
-    A class representing an agent implementing MGM-2 algorithm
-    """
-
-    def __init__(self, agent_id, constraints, domain, offer_prob):
-
-        super(MGM2, self).__init__(agent_id, constraints, domain)
-
-        self._iteration_count: int = 1
-        self._neighbors_values: Dict[int, int] = {}
-        self._committed: bool = False
-        self._offer_prob: float = offer_prob
-        self._iteration_switcher: Dict[int, Callable] = {1: self._commit_offers,
-                                                         2: self._create_pairs,
-                                                         3: None,
-                                                         4: None,
-                                                         5: None,
-                                                         }
-
-    def iteration(self, mailer: Mailer):
-
-        # Execute iteration according to the counter
-        self._iteration_switcher[self._iteration_count](mailer)
-
-        # Update iteration counter
-        self._iteration_count += 1 if self._iteration_count < 5 else 1
-
-    def _commit_offers(self, mailer: Mailer):
-
-        # Get the current messages as neighbors' values
-        self._neighbors_values = {m.sender: m.content for m in mailer.get_messages(self.id)}
-
-        # Decide to offer or not
-        if np.random.random() < self._offer_prob:
-
-            self._committed = True
-
-            # Choose neighbor and send offer
-            neighbor = np.random.choice(self._neighbors)
-            self._send_offer(mailer, neighbor)
-
-        else:
-
-            self.committed = False
-
-    def _send_offer(self, mailer: Mailer, neighbor: int):
-
-        # Create the offer
-        offer = Offer(self._neighbors_values, self._constraints, self.value)
-
-        # Send the offer to the neighbor
-        mailer.deliver_message(self.id, neighbor, offer, 'offer')
-
-    def _create_pairs(self, mailer: Mailer):
-
-        # Get all offers
-        offers: Dict[int, Offer] = {m.sender: m.content for m in mailer.get_messages(self.id)}
-
-        # If the agent committed an offer -
-        # answer `no` to all offers
-        if self._committed:
-
-            # Create the response message
-            response = {'accept': False}
-
-            for sender in offers.keys():
-                mailer.deliver_message(self.id, sender, response, 'response')
-
-        # If the agent didn't committed an offer -
-        # choose the best offer and response with `yes`
-        else:
-
-            # Get the neighbor, gain & value which corresponded to the best offer
-            best_neighbor, gain, value = self._find_best_offer(offers)
-
-            # Accept the best offer reject all others
-            for sender in offers.keys():
-
-                # Create the response message according the sender
-                if sender == best_neighbor:
-
-                    response = {'accept': True, 'gain': gain, 'value': value}
-
-                else:
-
-                    response = {'accept': False}
-
-                # Send the message
-                mailer.deliver_message(self.id, sender, response, 'response')
-
-    def _find_best_offer(self, offers: Dict[int, Any]):
-
-        # todo: fill this up
-        pass
-
-
 class Offer:
     """
     Class for representing an offer of one agent to another
@@ -156,3 +59,117 @@ class Response:
 
         self.value: int = value
         self.gain: float = gain
+
+
+class MGM2(Agent):
+    """
+    A class representing an agent implementing MGM-2 algorithm
+    """
+
+    def __init__(self, agent_id, constraints, domain, offer_prob):
+
+        super(MGM2, self).__init__(agent_id, constraints, domain)
+
+        self._iteration_count: int = 1
+        self._neighbors_values: Dict[int, int] = {}
+        self._committed: bool = False
+        self._offer_prob: float = offer_prob
+        self._iteration_switcher: Dict[int, Callable] = {1: self._commit_offers,
+                                                         2: self._create_pairs,
+                                                         3: None,
+                                                         4: None,
+                                                         5: None,
+                                                         }
+
+    def iteration(self, mailer: Mailer):
+
+        # Execute iteration according to the counter
+        self._iteration_switcher[self._iteration_count](mailer)
+
+        # Update iteration counter
+        self._iteration_count += 1 if self._iteration_count < 5 else 1
+
+    def _commit_offers(self, mailer: Mailer):
+        """
+        Method for iteration #1.
+
+        The agent does the following steps:
+
+            1. Get his neighbors' values (using the messages from the last step)
+
+            2. Decide if to send an offer, if decide true - send an offer.
+
+        :param mailer: mailer for getting and sending messages
+        :return:
+        """
+        # Get the current messages as neighbors' values
+        self._neighbors_values = {m.sender: m.content for m in mailer.get_messages(self.id)}
+
+        # Decide to offer or not
+        if np.random.random() < self._offer_prob:
+
+            self._committed = True
+
+            # Choose neighbor
+            neighbor = np.random.choice(self._neighbors)
+
+            # Create & send the offer
+            offer = Offer(self._neighbors_values, self._constraints, self.value)
+            mailer.deliver_message(self.id, neighbor, offer, 'offer')
+
+        else:
+
+            self.committed = False
+
+    def _create_pairs(self, mailer: Mailer):
+        """
+        Method for iteration #2.
+
+        The agent does the following steps:
+
+            1. Get the received offers (using the messages from the last step)
+
+            2. Answer to the offers according the `committed` attribute from last step.
+
+        :param mailer: mailer for getting and sending messages
+        :return:
+        """
+        # Get all offers
+        offers: Dict[int, Offer] = {m.sender: m.content for m in mailer.get_messages(self.id)}
+
+        # If the agent committed an offer -
+        # answer `no` to all offers
+        if self._committed:
+
+            # Create the response message
+            response = Response(accept=False)
+
+            for sender in offers.keys():
+                mailer.deliver_message(self.id, sender, response, 'response')
+
+        # If the agent didn't committed an offer -
+        # choose the best offer and response with `yes`
+        else:
+
+            # Get the neighbor, gain & value which corresponded to the best offer
+            best_neighbor, gain, value = self._find_best_offer(offers)
+
+            # Accept the best offer reject all others
+            for sender in offers.keys():
+
+                # Create the response message according the sender
+                if sender == best_neighbor:
+
+                    response = Response(accept=True, gain=gain, value=value)
+
+                else:
+
+                    response = Response(accept=False)
+
+                # Send the message
+                mailer.deliver_message(self.id, sender, response, 'response')
+
+    def _find_best_offer(self, offers: Dict[int, Offer]):
+
+        # todo: fill this up
+        pass
