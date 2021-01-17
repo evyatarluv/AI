6cac215a6907cbaae6eeae52bde95efdb2e013ca
luvaton <evyatarluv@gmail.com>
2021-01-17 10:06:23 +0200

    finish with the first figure

--- reports/dcop_plots.py
+++ reports/dcop_plots.py
@@ -10,14 +10,28 @@ def compare_p2():
     # Read the results
     path = 'results/compare_p2/{}.pickle'
     dsa: Dict[float, List] = pickle.load(open(path.format('dsa'), 'rb'))
-    # mgm2 = pickle.load(open(path.format('mgm2'), 'rb'))
+    mgm2: Dict[float, List] = pickle.load(open(path.format('mgm2'), 'rb'))
     p2 = [round(v, 2) for v in dsa.keys()]
 
     # Plot
+    # fig, (dsa_ax, mgm2_ax) = plt.subplots(1, 2)
+
     # DSA
     dsa_means = [np.mean(lst) for lst in dsa.values()]
     dsa_stds = [np.std(lst) for lst in dsa.values()]
-    plt.errorbar(p2, dsa_means, yerr=dsa_stds, capsize=2.5, fmt='-o')
+    plt.errorbar(p2, dsa_means, yerr=dsa_stds, capsize=2.5, fmt='-o', label='DSA')
+
+    # MGM2
+    mgm2_means = [np.mean(lst) for lst in mgm2.values()]
+    mgm2_stds = [np.std(lst) for lst in mgm2.values()]
+    plt.errorbar(p2, mgm2_means, yerr=mgm2_stds, capsize=2.5, fmt='-o', label='MGM2')
+
+    # Aesthetic things
+    plt.grid(axis='y')
+    plt.legend()
+    plt.ylabel('Sum of Costs')
+    plt.xlabel(r'$p_2$')
+    plt.title(r'Compare $p_2$ Influence')
     plt.show()
 
 
